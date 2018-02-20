6aabc8fdfbb50a085ec58cf81343b0f2775100e1
Luis96791 <luis.fajardozap@hotmail.com>
2018-02-19 21:41:16 -0600

    creando partials

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -21,6 +21,20 @@ class LocationsController < ApplicationController
 		@locations = Location.all
 	end
 
+	def edit
+		@location = Location.find(params[:id])
+	end
+
+	def update
+		@location = Location.find(params[:id])
+
+		if @location.update(location_params)
+			redirect_to @location
+		else
+			render 'edit'
+		end
+	end
+
 	private
 		def location_params
 			params.require(:location).permit(:name)
